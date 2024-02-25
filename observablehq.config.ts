5e706049f908cdb1a6568774d47388e421134f01
Philippe Rivière <fil@rezo.net>
2024-02-25 15:28:49 +0100

    remove toc

--- observablehq.config.ts
+++ observablehq.config.ts
@@ -61,11 +61,8 @@ export default {
   // Some additional configuration options and their defaults:
   // theme: "default", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
-  // footer: "Built with Observable.", // what to show in the footer (HTML)
-  // toc: true, // whether to show the table of contents
-  // pager: true, // whether to show previous & next links in the footer
-  // root: "docs", // path to the source root for preview
-  // output: "dist", // path to the output root for build
+  toc: false, // whether to show the table of contents
+  pager: false, // whether to show previous & next links in the footer
   sidebar: true,
   search: true,
   footer: `
