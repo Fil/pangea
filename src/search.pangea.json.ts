f920696e10e2228b8a61239516b588897088d976
Philippe Rivière <fil@rezo.net>
2024-05-15 21:53:44 +0200

    include plot

--- src/search.pangea.json.ts
+++ src/search.pangea.json.ts
@@ -1,3 +1,7 @@
+// this file needs to be copied over from
+// "../plot/docs/.vitepress/dist/assets/chunks/@localSearchIndexroot.k5mZR14Z.js"
+import plotindex from "../src/.observablehq/cache/plotsearch.js";
+
 async function getMinisearchIndex(source: string) {
   const r1 = await fetch(`${source}_observablehq/search.js`);
   if (r1.ok) {
@@ -11,7 +15,11 @@ async function main() {
   const d3 = await getMinisearchIndex("https://d3.observablehq.cloud/examples/");
   const pangea = await getMinisearchIndex("https://observablehq.observablehq.cloud/pangea/");
   const framework = await getMinisearchIndex("https://observablehq.com/framework/");
-  process.stdout.write(JSON.stringify({d3, pangea, framework}));
+  const plot = {
+    source: "https://observablehq.com/",
+    index: {...JSON.parse(plotindex), options: {fields: ["title", "titles", "text"], storeFields: ["title"]}}
+  };
+  process.stdout.write(JSON.stringify({d3, plot, pangea, framework}));
 }
 
 main();
