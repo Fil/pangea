7b92f00e5dc91b8205dcbf6fa5bced65ab4a6595
Philippe Rivière <fil@rezo.net>
2024-05-16 13:49:36 +0200

    also index observable documentation

--- src/search.pangea.json.ts
+++ src/search.pangea.json.ts
@@ -1,8 +1,8 @@
 async function find(needle: string, ext: string, ref: string): Promise<string> {
-  const re = new RegExp(`\\b${needle}\\.\\w+\\.${ext}\\b`);
+  const re = new RegExp(`\\b${needle}\\.[a-z0-9_-]+\\.${ext}\\b`, "i");
   const haystack = await fetch(ref).then((resp) => resp.text());
   const m = re.exec(haystack);
-  if (!m) throw new Error(`can't find ${needle} in ${ref} (${haystack})`);
+  if (!m) throw new Error(`can't find ${needle} in ${ref}`);
   return m[0];
 }
 
@@ -21,19 +21,21 @@ async function getMinisearchIndexVP(ref: string, source = ref) {
   const searchbox = await find("VPLocalSearchBox", "js", `${ref}assets/chunks/${theme}`);
   const searchroot = await find("localSearchIndexroot", "js", `${ref}assets/chunks/${searchbox}`);
   const searchindex = await fetch(`${ref}assets/chunks/@${searchroot}`).then((d) => d.text());
-  return {
-    source,
-    index: JSON.parse(searchindex.match(/'(.*)'/m)![1])
-  };
+  const e = eval(searchindex.replace(/^\s*const \w+=/, "").replace(/;export{\w+ as default};\s*$/, ""));
+  return {source, index: JSON.parse(e)};
 }
 
 async function main() {
+  const documentation = await getMinisearchIndexVP(
+    "https://observablehq.com/documentation/",
+    "https://observablehq.com"
+  );
   const d3 = await getMinisearchIndexFW("https://d3.observablehq.cloud/examples/");
+  const d3docs = await getMinisearchIndexVP("https://d3js.org/", "https://d3js.org");
   const framework = await getMinisearchIndexFW("https://observablehq.com/framework/");
   const pangea = await getMinisearchIndexFW("https://observablehq.observablehq.cloud/pangea/");
   const plot = await getMinisearchIndexVP("https://observablehq.com/plot/", "https://observablehq.com");
-  const d3docs = await getMinisearchIndexVP("https://d3js.org/", "https://d3js.org");
-  process.stdout.write(JSON.stringify({d3, d3docs, framework, pangea, plot}));
+  process.stdout.write(JSON.stringify({d3, d3docs, documentation, framework, pangea, plot}));
 }
 
 main();
