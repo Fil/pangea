e0e4c0416d72ab142a752db364f8a0ba506399ef
Philippe Rivière <fil@rezo.net>
2024-09-03 12:44:35 +0200

    embed video; it doesn't help with the thumbnails though

--- src/video/[youtubeid].md.ts
+++ src/video/[youtubeid].md.ts
@@ -13,6 +13,8 @@ const link = `https://www.youtube.com/watch?v=${youtubeid}`;
 const dateFormat = utcFormat("%Y-%m-%d");
 const parser = new Parser({customFields: {item: ["title", "media:group", "isoDate"]}});
 
+// This could use a cache or a chained data loader instead of loading the same
+// RSS file for each video.
 const url = "https://www.youtube.com/feeds/videos.xml?channel_id=UCCD2tAKN32ya7V639gkbWhg";
 const entry = await parser.parseURL(url).then((feed) => feed.items.find((entry) => entry.link === link));
 
@@ -31,7 +33,8 @@ ignore_head: '<meta http-equiv="refresh" content="0; url=${link}">'
 # ${title}
 ## Published ${dateFormat(new Date(entry.isoDate))}
 
-<img class=thumbnail src=${JSON.stringify(thumbnail)} style="width: 640px; max-width: 100%;">
+<!-- <img class=thumbnail src=${JSON.stringify(thumbnail)} style="width: 640px; max-width: 100%;"> -->
+<iframe class=thumbnail style="max-width: 100%" width="640" height="360" src="https://www.youtube.com/embed/${youtubeid}" title="YouTube video player" frameborder="0" allow="autoplay; encrypted-media; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
 <small>
 
