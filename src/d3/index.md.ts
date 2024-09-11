9283bddc28f4df896c9ce249d359ea73b6b70b31
Philippe Rivière <fil@rezo.net>
2024-09-11 23:04:25 +0200

    no 'more' on d3/

--- src/d3/index.md.ts
+++ src/d3/index.md.ts
@@ -7,4 +7,4 @@ const intro = `# D3 gallery
 Looking for a good D3 example? Here’s a few (okay, ${count.toLocaleString("en")}…) to peruse.
 `;
 
-process.stdout.write(await createIndex(sections as any, intro, "More…"));
+process.stdout.write(await createIndex(sections as any, intro, ""));
